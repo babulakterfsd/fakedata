3703cec7e63d0b7b054b5819b366d5439052a5a7
babulakterfsd <babulakterfsd@gmail.com>
2021-09-23 12:33:54 +0600

    all functionalities added

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -10,12 +10,56 @@ const products = ({products}) => {
 };
 
 const SingleProduct = ({SingleProduct}) => {
+  const handlePurchase = product => {
+    let {id,name,price} = product;
+    let cart = {};
+    const exists = localStorage.getItem('shopping-cart');
+    if(!exists) {
+       cart[id] = 1
+    } else {
+       cart = JSON.parse(exists)
+        if(cart[id]) {
+          const quantity = cart[id] + 1;
+          cart[id] = quantity
+        } else {
+          cart[id] = 1
+        }
+    }
+    localStorage.setItem('shopping-cart', JSON.stringify(cart));
+    (function calculateCart() {
+      price = price.slice(1, price.length)
+      const productPrice = +price;
+      const productQuantity = +cart[id];
+      const total = productPrice * productQuantity;
+      alert(`You added ${name} to your cart ${cart[id]} times, Bill is ${productPrice} * ${productQuantity} = ${total.toFixed(2)}`)
+    })()
+  }
+
+  const handleRemove = product => {
+     const {id,name} = product;
+     const exists = localStorage.getItem('shopping-cart')
+     if(!exists) {
+          alert('Lol ! You did not buy anything');
+     } else {
+        const cart = JSON.parse(exists)
+        delete cart[id];
+        alert(`${name} removed from your cart`)
+        localStorage.setItem('shopping-cart', JSON.stringify(cart))
+        if(Object.keys(cart).length === 0) {
+          localStorage.clear()
+        }
+     }
+  }
+
+
+
   return (
       <div>
          <div className="singleProduct">
            <h6 className="text-center">{SingleProduct.name}</h6>
            <h4 className="text-center">{SingleProduct.price}</h4>
-           <button className="btn btn-success d-block mx-auto px-4 fw-bold">Buy</button>
+           <button onClick={() => handlePurchase(SingleProduct)} className="btn btn-success d-block mx-auto px-4 fw-bold mb-2">Add to Cart</button>
+           <button onClick={() => handleRemove(SingleProduct)} className="btn btn-danger d-block mx-auto px-4 fw-bold">Remove</button>
          </div>
       </div>
   )
